42ce88020c39ddb39fdcb7f83091dd0784b77b47
Fahima Akther Liza <lizafahima660@gmail.com>
2026-04-19 00:05:42 +0600

    change a line

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 app.use('/api/reach', reachRoutes);
 app.use('/api/alumni', alumniRoutes);
 app.use('/api/teachers', teacherRoutes);
-app.use('/api/feedback', require('./routes/feedbackRoutes'));
+app.use('/api/feedback', require('./routes/FeedbackRoutes'));
 
 // Basic Route
 app.get('/', (req, res) => {
